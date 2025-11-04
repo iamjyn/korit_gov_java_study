b1ff2566723e50fc2da1d712e42d773ba52cf21a
iamjyn <iam_jyn@naver.com>
2025-11-04 11:38:06 +0900

    Feat: StringBuilder

---
+++ src/_31_StringBuilder/StringBuilder01.java
@@ -0,0 +1,44 @@
+package _31_StringBuilder;
+
+public class StringBuilder01 {
+    public static void main(String[] args) {
+        // String(final,불변)
+        // StringBuffer(가변) : 병렬연산해야하는 스레드 환경에서 안정적
+        // StringBuilder(가변) : 연산속도가 제일 빠름 (단일 스레드 환경에서 주로 사용)
+        // 빌더랑 버퍼 메소드 똑같음
+
+        String str = "abcdefg";
+        StringBuilder sb = new StringBuilder(str);
+        StringBuilder sb2 = new StringBuilder("abcdefg");
+        System.out.println(sb);
+        System.out.println(sb == sb2);
+        System.out.println(sb.equals(sb2));
+        System.out.println(sb.toString().equals(sb2.toString()));
+
+        // String 변환 / StringBuilder -> String
+        System.out.println("문자열 String 변환: " + sb.toString());
+
+        // 문자열 추출
+        System.out.println("문자열 추출: " + sb.substring(2, 4));
+
+        // 문자열 추가
+        System.out.println("문자열 추가: " + sb.insert(2, "추가"));
+
+        // 문자열 삭제
+        System.out.println("문자열 삭제: " + sb.delete(2, 4));
+
+        // 문자열 연결(덧셈연산)
+        System.out.println("문자열 연결: " + sb.append("hijk"));
+
+        // 문자열 길이
+        System.out.println("문자열 길이: " + sb.length());
+
+        // 용량의 크기
+        System.out.println("용량의 크기: " + sb.capacity());
+
+        // 문자열 뒤집기
+        System.out.println("문자열 뒤집기: " + sb.reverse());
+
+
+    }
+}
---
+++ src/_31_StringBuilder/StringBuilder02.java
@@ -0,0 +1,46 @@
+package _31_StringBuilder;
+
+public class StringBuilder02 {
+    public static void main(String[] args) {
+        final int lengths = 200000;
+
+        // String 시간
+        long startTime1 = System.currentTimeMillis(); // 시작
+
+        String str = "";
+        for (int i = 0; i < lengths; i++) {
+            str = str + "*";
+        }
+
+        long endTime1 = System.currentTimeMillis(); // 끝
+        long duration = endTime1 - startTime1;
+        System.out.println("String 시간: " + duration);
+
+
+        // StringBuffer 시간
+        long startTime2 = System.currentTimeMillis(); // 시작
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < lengths; i++) {
+            sb.append("*");
+        }
+
+        long endTime2 = System.currentTimeMillis(); // 끝
+        long duration2 = endTime2 - startTime2;
+        System.out.println("StringBuffer 시간: " + duration2);
+
+
+        // StringBuilder 시간
+        long startTime3 = System.currentTimeMillis(); // 시작
+
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < lengths; i++) {
+            sb2.append("*");
+        }
+
+        long endTime3 = System.currentTimeMillis(); // 끝
+        long duration3 = endTime3 - startTime3;
+        System.out.println("StringBuilder 시간: " + duration3);
+
+    }
+}
